9312ff4980145afd9b47a0e6f73fa1427360af53
ayaangazali <ayaangazali.work@gmail.com>
2021-02-27 11:48:14 -0800

    products.js catalog

---
+++ products.js
@@ -0,0 +1,30 @@
+var products = [
+	{
+		id: 1,
+		name: "Posture Corrector",
+		price: 19.99,
+		category: "Fitness",
+		image: "images/posture.jpg"
+	},
+	{
+		id: 2,
+		name: "LED Strip Lights",
+		price: 24.99,
+		category: "Home",
+		image: "images/led.jpg"
+	},
+	{
+		id: 3,
+		name: "Mini Drone",
+		price: 49.99,
+		category: "Gadgets",
+		image: "images/drone.jpg"
+	},
+	{
+		id: 4,
+		name: "Portable Blender",
+		price: 29.99,
+		category: "Home",
+		image: "images/blender.jpg"
+	}
+];
